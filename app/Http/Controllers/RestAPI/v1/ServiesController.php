a24b55e55ff2c080eec5c2ee9e4a77153f94c49d
Mohamed Galal <mgalal8866@gmail.com>
2024-06-03 16:33:58 +0300

    modified:   app/Http/Controllers/RestAPI/v1/ServiesController.php

--- app/Http/Controllers/RestAPI/v1/ServiesController.php
+++ app/Http/Controllers/RestAPI/v1/ServiesController.php
@@ -43,7 +43,7 @@ class ServiesController extends Controller
             $data=['body'=>$request->body];
 
             $data=['phone'=> $request->phone];
-            
+
         try {
             $booking_servies =  Services::create($data);
 
